50fd4a44f9d007c7aaba4d493fabe11b7b0ed886
Raiane <hihae1311@gmail.com>
2025-04-04 00:45:38 -0300

    Adicionar método para inserir dados a partir de um arquivo CSV na tabela de agentes

--- models/database.py
+++ models/database.py
@@ -1,5 +1,6 @@
 import sqlite3  # ou outro banco
 import models.connection as database
+import csv
 
 db = database.conexao()
 
@@ -12,6 +13,16 @@ class Database:
     def execute_script(self, script_path):
         with open(script_path, 'r') as f:
             self.conn.executescript(f.read())
+
+    def insert_from_csv(self, csv_path):
+        with open(csv_path, 'r', encoding='utf-8') as file:
+            reader = csv.DictReader(file)
+            for row in reader:
+                self.db.cursor.execute(
+                    "INSERT INTO agents (name, description) VALUES (?, ?)",
+                    (row['name'], row['description'])
+                )
+        self.db.conn.commit()
 
     def close(self):
         self.conn.close()
